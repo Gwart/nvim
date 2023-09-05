82dbb6b82bedefc379f9a5173026459ced9a0dac
Gwart <delomik@mail.ru>
2023-09-05 20:15:39 +0200

    remove devicons for nvim-tree

--- lua/gwart/nvimtreeconf.lua
+++ lua/gwart/nvimtreeconf.lua
@@ -5,19 +5,32 @@ function nvimtreeconf.setup()
 	require("nvim-tree").setup({
     renderer =
     {
-        icons =
-        {
-            web_devicons =
+        icons = {
+            show =
             {
-                file =
-                {
-                    enable = false,
-                    color = false,
-                },
+                file = true,
+                folder = true,
+                folder_arrow = false,
+                git = false,
+                diagnostics = false,
+                modified = true,
+            },
+            glyphs =
+            {
+                default = "",
+                symlink = "s",
+                bookmark = "",
+                modified = "m",
                 folder =
                 {
-                    enable = false,
-                    color = false,
+                    arrow_closed = ">",
+                    arrow_open = "v",
+                    default = "d",
+                    open = "do",
+                    empty = "d",
+                    empty_open = "do",
+                    symlink = "s",
+                    symlink_open = "so",
                 },
             },
         },
